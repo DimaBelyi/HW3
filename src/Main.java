b2d3459dfaec1b1e90ab19c217eb84707d5bc9ff
DimaBelyi <dima_belyi@mail.ru>
2024-09-18 02:42:47 +0500

    HW3

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
         int a = 1;
         byte b = 2;
         short c = 3;
-        long d = 4;
-        float e = 5;
-        double f = 6;
+        long d = 4L;
+        float e = 5.5f;
+        double f = 6.66;
         System.out.println("Значение переменной a с типом int равно " + a);
         System.out.println("Значение переменной b с типом byte равно " + b);
         System.out.println("Значение переменной c с типом short равно " + c);
@@ -16,10 +16,10 @@ public class Main {
         System.out.println(" ");
 //Задача №2
         float aa = 27.12f;
-        var bb = "987 678 965 549";
+        long bb = 987_678_965_549L;
         double cc = 2.786;
         short dd = 569;
-        short ee = -159;
+        int ee = -159;
         int ff = 27897;
         byte gg = 67;
 //Задача №3
@@ -30,17 +30,18 @@ public class Main {
         int totalStudents = lpStudents + asStudents + eaStudents;
         int sheetStudent = sheetTotal / totalStudents;
         System.out.println("На каждого ученика рассчитано " + sheetStudent + " листов бумаги");
+        System.out.println();
 //Задача №4
-        byte bottleMinute=16/2;
-        int bottle20Minute=20*bottleMinute;
-        int bottleDay=1440*bottleMinute;
-        int bottle3Day=4320*bottleMinute;
-        int bottleMomth=43200*bottleMinute;
-        System.out.println("За 20 минут машина произвела "+bottle20Minute+" штук бутылок");
-        System.out.println("За сутки машина произвела "+bottleDay+" штук бутылок");
-        System.out.println("За 3 дня машина произвела "+bottle3Day+" штук бутылок");
-        System.out.println("За месяц машина произвела "+bottleMomth+" штук бутылок");
-        System.out.println(" ");
+        int bottleMinute = 16 / 2;
+        int bottle20Minute = 20 * bottleMinute;
+        int bottleDay = 60 * 24 * bottleMinute;
+        int bottle3Day = 3 * bottleDay;
+        int bottleMomth = 30 * bottleDay;
+        System.out.println("За 20 минут машина произвела " + bottle20Minute + " штук бутылок");
+        System.out.println("За сутки машина произвела " + bottleDay + " штук бутылок");
+        System.out.println("За 3 дня машина произвела " + bottle3Day + " штук бутылок");
+        System.out.println("За месяц машина произвела " + bottleMomth + " штук бутылок");
+        System.out.println();
 
     }
 }
